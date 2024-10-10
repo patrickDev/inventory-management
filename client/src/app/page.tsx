bd35e8c817e400513af8c0143bdab0bfe6bf50e9
Patrick Ngueguim <patrickngueguim@Patricks-MBP.attlocal.net>
2024-10-09 22:28:17 -0500

    first commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+      <h1>Main</h1>
+      </main>
+     
+    </div>
+  );
+}
