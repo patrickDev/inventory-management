ced0bbd1113a1bfacdadb1113dd29c272b36f84d
Patrick Ngueguim <patrickngueguim@Patricks-MBP.attlocal.net>
2024-10-27 00:19:50 -0500

    update product page

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -81,7 +81,7 @@ const Products = () => {
             >
               <div className="flex flex-col items-center">
               <Image
-                    src={`https://s3-inventorymanagement12.s3.us-east-2.amazonaws.com/${Math.floor(Math.random() * 3 + 1)}.png`}
+                    src={`https://s3-inventorymanagement12.s3.us-east-2.amazonaws.com/product${Math.floor(Math.random() * 3 + 1)}.png`}
                     alt={product.name}
                     width={149}
                     height={150}
