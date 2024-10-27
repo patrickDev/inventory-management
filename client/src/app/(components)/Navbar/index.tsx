0061aac99d809b6c5999ff7c8f2bba2c8ef9944c
Patrick Ngueguim <patrickngueguim@Patricks-MBP.attlocal.net>
2024-10-27 01:01:09 -0500

    update profile page

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -60,7 +60,7 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
            <Image
-              src="https://s3-inventorymanagement12.s3.us-east-2.amazonaws.com/profile.png"
+              src="https://s3-inventorymanagement12.s3.us-east-2.amazonaws.com/profile.jpg"
               alt="profile"
               width={50}
               height={50}
